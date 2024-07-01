e80badb67d5d2d2e1783a6988e480cd8b7c277f4
Alrn <146360711+Annabelle-Lrn@users.noreply.github.com>
2024-07-01 12:06:15 +0200

    #

--- script.js
+++
@@ -1,6 +0,0 @@
-function showCarrousel(diapo){
-    const diapoimg = document.getElementById(diapo);
-    diapoimg.append(diapoimg.firstElementChild);
-    setTimeout(()=> showCarrousel(diapo),6000);
-}
-showCarrousel("diapo")
