380dd304f7f10a6fbee44ce7ff51b01d73f09eac
Alrn <146360711+Annabelle-Lrn@users.noreply.github.com>
2024-07-02 12:45:58 +0200

    #

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ const descrip = document.querySelectorAll(".description");
 const modalImg =document.querySelector("#modal img");
 const modal= document.getElementById("modal");
 const close = document.querySelector(".close");
+const legende = document.querySelector("#modal p");
+const pdiv = document.querySelector(".description");
+// console.log(pdiv.textContent)
 let index =0;
-console.log(items);
+
 
 function showItems(){
     if (index>=items.length-1){index=0}
@@ -19,15 +22,19 @@ function showItems(){
 
     items[index].classList.add("active");
     descrip[index].classList.add("active");
+    
 };
 
 let t = setInterval(()=> {showItems()},3000);
 
-items.forEach(itemi => {
-    itemi.addEventListener("click",(e) =>{
+items.forEach(item => {
+    item.addEventListener("click",(e) =>{
         const img =e.target.src;
+        const descr = e.target.alt;
         modalImg.src= img;
+        legende.innerHTML=descr;
         modal.classList.add("show");
+        pdiv.classList.add("show")
         clearInterval(t);
     })
 });
