13e89b8607931cd19022b6f378593033f71eeab0
Alrn <146360711+Annabelle-Lrn@users.noreply.github.com>
2024-08-29 08:31:52 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -9,6 +9,39 @@ const swiper = new Swiper(".swiper",{
     },
 })
 
-// swiper.slideNext(5000, runCallbacks)
-// const swiperSlides = document.querySelector(".swiper-slide");
-// swiperSlides.slideNext();
+const form = document.querySelector('form');
+
+form.addEventListener('submit',(e)=> {
+    e.preventDefault();
+    let errors=[ ];
+    if (nameInput.value.trim() === ''){
+        errors.push('Veuillez saisir votre nom.');
+    }else if (nameInput.value.trim().length <4){
+        errors.push("le nom doit contenir au moins 3 caractères");
+    }
+    
+    if (email.value.trim() === ''){
+        errors.push ("Veuillez saisir votre email.");
+    }else if (!isValidEmail(email.value)){
+        errors.push("Veuillez saisir une adresse email valide.");
+    }
+})
+
+function isValidEmail(email) {
+    const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return re.test(email);
+}
+
+function displayErrors(errors) {
+    const elError = document.querySelectorAll(".error");
+    elError.forEach(el =>{ el.remove()});
+
+    const errorContainer = document.createElement("div");
+    errorContainer.classList.add('error');
+    errors.forEach(error =>{
+        const errorMsg = document.createElement('p');
+        errorMsg.textContent = error;
+        errorContainer.appendChild(errorMsg);
+    })
+    form.appendChild(errorContainer);
+}
