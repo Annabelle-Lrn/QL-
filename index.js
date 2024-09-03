c8a6b406eaae965d0c749f4504947f9b800e41ef
Alrn <146360711+Annabelle-Lrn@users.noreply.github.com>
2024-09-03 12:14:19 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -7,41 +7,33 @@ const swiper = new Swiper(".swiper",{
         el :".swiper-scrollbar",
     
     },
+});
+// Fléche pour remonter
+arrowUp.addEventListener('click',() =>{
+  window.scrollTo({
+    top:0,
+    left:0,
+    behavior:'smooth',
+  })
+});
+
+window.addEventListener("scroll", ()=> {
+  let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+
+
+  if (scrollValue >= 0.51){
+    arrowUp.style.opacity=1;
+    if(scrollValue >=0.9){
+      arrowUp.style.bottom="150px";
+    }
+  }else{
+    arrowUp.style.opacity=0;
+  }
+});
+
+window.addEventListener("submit",(e)=>{
+  if(!accord.checked){
+    alert("Veuillez cocher la case pour que je puisse vous contacter");
+  }
 })
 
-const form = document.querySelector('form');
-
-form.addEventListener('submit',(e)=> {
-    e.preventDefault();
-    let errors=[ ];
-    if (nameInput.value.trim() === ''){
-        errors.push('Veuillez saisir votre nom.');
-    }else if (nameInput.value.trim().length <4){
-        errors.push("le nom doit contenir au moins 3 caractères");
-    }
-    
-    if (email.value.trim() === ''){
-        errors.push ("Veuillez saisir votre email.");
-    }else if (!isValidEmail(email.value)){
-        errors.push("Veuillez saisir une adresse email valide.");
-    }
-})
-
-function isValidEmail(email) {
-    const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return re.test(email);
-}
-
-function displayErrors(errors) {
-    const elError = document.querySelectorAll(".error");
-    elError.forEach(el =>{ el.remove()});
-
-    const errorContainer = document.createElement("div");
-    errorContainer.classList.add('error');
-    errors.forEach(error =>{
-        const errorMsg = document.createElement('p');
-        errorMsg.textContent = error;
-        errorContainer.appendChild(errorMsg);
-    })
-    form.appendChild(errorContainer);
-}
--- index.php
+++ index.php
@@ -1,113 +1,197 @@
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>QL Elect - Electricien à Saint Gervais d'auvergne</title>
-    <meta charset="utf-8" />
-    <link rel="stylesheet" href="./assets/styles/index.css">
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
-      integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    />
-    
-  </head>
-  <body>
-    <header>
-      <h1>QL Elec</h1>
-    </header>
-    <main>
-      <div class="info-dev">
-        <p>Secteur d'intervention :<br> rayon de 20km autour de St Gervais d'Auvergne</p>
-        <p class="right">Devis gratuit</p>
-      </div>
-      <div class="grid-container">
-          <section class="presta-grid">   
-            <div class="prestation-container">
-              <h2>Prestations</h2>
-              <ul>
-                <div class="puce">
-                  <i class="fa-regular fa-lightbulb"></i>
-                  <li>Installation éléctrique</li>
-                </div>
-                <div class="puce">
-                  <i class="fa-regular fa-lightbulb"></i>
-                  <li>Rénovation éléctrique</li>
-                </div>  
-                <div class="puce">
-                  <i class="fa-regular fa-lightbulb"></i>
-                  <li>Dépannage</li>
-                </div>
-                <div class="puce">
-                  <i class="fa-regular fa-lightbulb"></i>
-                  <li>Mise aux normes</li>
-                </div>
-              </ul>
-            </div>
-          </section>
-          <section class="carrousel-grid">
-          <div id="diapo" class="carrousel">
-             <img class="item"src="./assets/img/tableauavant1.webp" alt="tableau éléctrique avant travaux">
-            <img id="item" src="./assets/img/tableauapres1.webp" alt="tableau éléctrique aprés travaux">
-            <img class="item" src="./assets/img/creationtableau1.webp" alt="création tableau">
-          </div>
-         </section>        
-          <section class="contact-grid">
-            <div class="contact-form">
-              <form action="POST" action="mail.php">
-              <h2>Contactez-moi</h2>
-              <div class="corpsformulaire">
-                <div class="boite">
-               <label for="nom">Votre nom</label>
-                   <i class="fa-solid fa-user" style="color: #103d74;"></i>
-                   <input type="text" name="nom" id="nom" required>
-              </div>
-              <div class="boite">
-               <label for="telephone">Votre numéro de téléphone</label>
-               <i class="fa-solid fa-phone" style="color:#103d74;"></i>
-              <input type="text" name="telephone" id="telephone" required>
-              </div>
-              <div class="boite">
-             <label for="mail">Votre email</label>
-              <i class="fa-solid fa-envelope" style="color: #103d74;"></i>
-              <input type="email" name="email" id="mail" required>
-              </div>
-              <div class="boite">
-                      <label for="demande">Votre demande</label>
-                      <select id="demande">
-                        <option value="">Choisissez un motif</option>
-                        <option value="Miseaunormes">Mise aux normes</option>
-                        <option value="reno">Rénovation</option>
-                        <option value="nouvelleinstallation">Nouvelle installation</option>
-                        <option value="acquisition">Demande de devis pour un futur achat</option>
-                      </select>
-                </div>
-                    <div class="boite">
-                      <label for="message" name="message">Message</label>
-                      <textarea id="message"placeholder="Saisissez votre message ici..."></textarea>
-                    </div>
-                  </div>                    
-              <input class="btn" type="submit">
-            </form>
-            <?php
-            if (isset($_POST['message'])) {
-                $retour = mail('qlelec@outlook.com', 'Envoi depuis la page Contact', $_POST['message'], 'From: webmaster@monsite.fr' . "\r\n" . 'Reply-to: ' . $_POST['email']);
-                if($retour)
-                    echo '<p>Votre message a bien été envoyé.</p>';
-            }
-            ?>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="./assets/logoF.png">
+  <title>QL Elec - Electricien à Saint Gervais d'Auvergne</title>
+  <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="./assets/styles/index.css">
+</head>
+<body>
+<div class="start_image">
 
-          </div>  
-          </section>
+</div>
+<header> 
+  <div class="devis_gratuit">
+    <h3>Devis Gratuit</h3>
+  </div>
+  <div class="entete">
+    <h1>QL Elec</h1>
+    <h2>Electricien à Saint Gervais dAuvergne</h2>
+    <h3>06.29.95.99.77 | QLelec@outlook.com</h3>
+  </div>
+  <div class="deplacement">
+    <h3>Interventions : <br> rayon de 20Km</h3>
+  </div>
+</header>
+<main>
+  <nav id="navbar">
+    <ul>
+      <li> <a href="#a_propos">A propos</a>  </li>
+      <li> <a href="#services">Prestations</a>  </li>
+      <li> <a href="#realisations">Réalisations</a> </li>
+      <li> <a href="#form_contact">Me contacter</a> </li>
+    </ul>
+  </nav>
+  <div id="a_propos" class="propos">
+    <div class="presentation">
+      <p>J'interviens principalement à saint gervais d'auvergne et dans un rayon de 20 km.
+        <br>En choisissant mes services, vous opter pour un électricien qui allie technique, précision et un sens aigu du service. <br>Je suis à votre écoute pour tous vos projets électriques, que ce soit pour une rénovation, une installation neuve ou un dépannage. </p>
+    </div>
+    <div class="objectif">
+      <p>Mon objectif : Vous fournir une prestation de qualité, qui respecte vos attentes et vos délais.</p>
+    </div>
+  </div>
+  <div id="arrowUp">
+  <i class="fa-solid fa-angles-up"></i>
+  </div>
+  </div>
+  <section class="services_carrousel">
+    <div id="services">
+      <div class="card-service">
+        <img src="./assets/img/ampoules.png" alt="">
+        <p>Installation / Rénovation <br> du systèmes d'éclairage</p>
+      </div>
+      <div class="card-service">
+        <img src="./assets/img/tableau.png" alt="">
+        <p>Installation/ Rénovation <br> de l'installation électrique<br>(prises, interupteurs, tableaux-électriques)</p>
+      </div>
+      <div class="card-service">
+        <img src="./assets/img/prise.png" alt="">
+        <p>Dépannage<br> (pannes, courts-circuits)</p>
+      </div>
+      <div class="card-service">
+        <img src="./assets/img/multimetre.png" alt="">
+        <p>Mise en conformité</p>
+      </div>
+
+    </div>
+
+    <div class="swiper" id="realisations">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">
+          <img src="./assets/img/carrefour.webp" alt="Changement de luminaire à Carrefour St Gervais">
+          <p> Changement de luminaire à Carrefour St Gervais</p>
         </div>
-    </main>
-    <footer>
-      <p class="ste edosz">EURL QL ELEC</p>
-      <p class="edosz">Contact</p><p> 06.29.95.99.77 | </p><p class="edosz">Mail</p>: QLelec@outlook.com</p>
-    </footer>
-    <script src="script.js"></script>
-  </body>
+        <div class="swiper-slide">
+          <img src="./assets/img/creationtableau1.webp" alt="Création tableau électrique">
+          <p>Création tableau électrique</p>
+        </div>
+        <div class="swiper-slide">
+          <img src="./assets/img/landogne.webp" alt="Intervention à Landogne">
+          <p>Intervention à Landogne</p>
+        </div>
+        <div class="swiper-slide">
+          <img src="./assets/img/tableau_avant_apres.webp" alt="Tableau électrique avant/après">
+         <p> Mise au normes tableau</p>
+        </div>
+        <!-- <div class="swiper-slide"></div> -->
+      </div>
+      <div class="swiper-scrollbar"></div>
+    </div>
+
+  </section>
+
+  <section id="form_contact">
+    <h3>Me contacter</h3>
+    <?php
+    $erreurs =['nom'=>" ", 'prenom'=> " ", 'mail'=> " ", 'telephone'=>" ",'sujet'=>" ", 'message'=>" " ];
+    $confirmeMailenvoi =" ";
+    $visibiliteForm = " ";
+
+    if(isset($_GET['statut']) && $_GET['statut'] == 'succes'){
+      $confirmeMailenvoi =" Votre message a bien été enoyé. Je prendrai contact avec vous dans les meilleurs délais.";
+      $visibiliteForm = false;
+    }elseif(isset($_GET['statut']) && $_GET['statut'] == 'erreur'){
+      $erreurs =[
+        'nom' => $_GET['er_nom']??'',
+        'prenom' => $_GET['er_prenom']??'',
+        'mail' => $_GET['er_mail']??'',
+        'telephone' => $_GET['er_telephone']??'',
+        'sujet'=> $_GET['er_sujet']?? '',
+        'message' => $_GET['er_message']??''
+      ];
+    }
+    ?>
+    <?php if($confirmeMailenvoi): ?>
+      <p class="msgEnvoye"><?=$confirmeMailenvoi;?></p>
+    <?php endif;  ?>
+
+    <?php if($visibiliteForm): ?>
+    <form method="POST" action="/traitement.php">
+      <div class="form_id">
+        <label for="nom">Nom</label>
+        <input class="left" type="text" name="nom" id="nom" placeholder="Votre nom"required>
+        <span class="erreur"><?= $erreurs['nom']?? '';?></span>
+      </div>
+
+      <div class="form_id right">
+        <label for="prenom">Prénom</label>
+        <input type="text" name="prenom" id="prenom" placeholder="Votre prénom"required>
+      </div>
+
+      <div class="form_id">
+        <label for="mail">Email</label>
+        <input class="left" type="email" name="mail" id="mail" placeholder="Votre email" required>
+        <span class="erreur"><?= $erreurs['mail']?? '';?></span>
+      </div>
+      <div class="form_id  right">
+        <label for="telephone">Téléphone</label>
+        <input type="number" id="telephone"name="telephone" placeholder="Votre numéro de téléphone" required>
+        <span class="erreur"><?= $erreurs['telephone']?? '';?></span>
+      </div>
+        <label for="sujet"></label>
+        <select name="sujet" id="sujet">
+          <option value="">Sélectionner</option>
+          <option value="Demande de devis">Demande de devis</option>
+          <option value="Remise aux normes">Remise aux normes</option>
+          <option value="Depannage">Dépannage</option>
+        </select>
+           
+        <textarea name="message" id="message" placeholder="Saissez ici, votre message. Dans le cas d'un devis pour une vente ou une acquision merci de précier la date butoire et vos diposniblités"></textarea>
+        <span class="erreur"><?= $erreurs['message']?? '';?></span>
+      
+      
+      <input type="submit" name="envoyer" value="Envoyer">
+      <span id="erreur"> </span>
+
+      <div >
+        <p>En soumettant ce formulaire, vous accepter que la société QL Elec mémorise et utilise vos données personnelles afin de pouvoir vous contacter. Vos coordonnées ne seront transmises à aucun tiers, ni utilisées pour de la publicité.</p>
+        <input type="checkbox" id="accord">
+        <label for="accord">J'autorise la société QL Elec à communiquer avec moi</label>
+      </div>
+    </form>
+    <?php endif; ?>
+  </section>
+  
+</main>
+<footer>
+  <div class="contact">
+    <h4> QL Elec</h4>
+    <div class="contact_container">
+      <div>
+        <h5>Téléphone :</h5> 
+        <p>06.29.95.99.77</p>
+      </div>
+      <div>
+        <h5>Mail :</h5>
+        <p>Qlelec@outlook.com</p>
+      </div>
+    </div>
+  </div>
+  <div class="mentions">
+    <ul>
+      <li>Mentions Légales</li>
+      <li> <a href="./cdv.html">Conditions Générales de Ventes</a></li>
+      <li> Réalisation : Annabelle Larney</li>
+    </ul>
+  </div>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<script src="/index.js"></script>
+</body>
 </html>
